a0fed7b2c9aa387b2ce1129448b1ba26064e21f8
Yusuke SUGAMIYA <yusuke.dnpp@gmail.com>
2024-09-02 12:43:59 +0900

    osc52copy and tmux ...

--- osc52copy.sh
+++ osc52copy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 # パイプで受け取った文字列を base64 にして OSC 52 でクリップボードにコピーする。
-printf "\e]52;c;%s\a" "$(printf "%s" "$*" | base64 -w 0)"
+content="$(cat /dev/stdin)"
+if [ -z "${content}" ]; then
+    exit
+fi
+b64content="$(echo "${content}" | base64 -w 0)"
+printf "\e]52;c;%s\a" "${b64content}"
